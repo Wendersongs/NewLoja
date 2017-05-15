c6c8c236a49a9c2824922d81ae0406fa44554c90
WENDERSON <WENDERSON@192.168.43.236>
2017-05-15 19:21:57 -0300

    Corrigido o driver do banco.

--- src/main/java/teste/Teste.java
+++ src/main/java/teste/Teste.java
@@ -15,7 +15,7 @@ public class Teste {
     public static void main(String[] args) {
        /// List<Cidade> listaCidade = CidadeDAO.listagem(null);
     //	System.out.print( System.getProperty("user.dir"));
-    	System.out.println(FacesContext.getCurrentInstance().getExternalContext().getRealPath("")+ "/resources/relatorios/");
+    //	System.out.println(FacesContext.getCurrentInstance().getExternalContext().getRealPath("")+ "/resources/relatorios/");
     
     }
 	
